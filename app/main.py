dc59c30e5be0bf31ef36736749746ae3e2953e4a
conicorreag <conicorreag@uc.cl>
2023-09-23 18:24:47 -0300

    arreglo cors

--- app/main.py
+++ app/main.py
@@ -6,14 +6,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    # Otras URL permitidas si es necesario
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
